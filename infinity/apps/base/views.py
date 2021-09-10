56499c8678f4c0c00e381463c3286cf29d999031
Parth Agarwal <agarwal.parth.101@protonmail.com>
2021-09-10 13:00:55 +0530

    The Highly Unnecessary Dynamic Serializer

--- infinity/apps/base/views.py
+++ infinity/apps/base/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseServerError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+from rest_framework.serializers import Serializer, CharField
 
 from .constants import Readme
 
@@ -23,10 +24,26 @@ def readme_view(request):
     except Exception:
         return HttpResponseServerError()
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=["GET"])
 def live_view(request, live_url_endpoint):
+
+    # 404 for incorrent url param
     if live_url_endpoint != live_settings.LIVE_URL_ENDPOINT:
-        raise NotFound()
-    return Response({
-        live_settings.LIVE_KEY: live_settings.LIVE_VALUE
-    })
+        raise NotFound(
+            f"Given URL param <live_url_endpoint:{live_url_endpoint}> "
+            "does not match with constance.config.LIVE_URL_ENDPOINT"
+        )
+
+    # commenting this part to make a unnecessary dynamic serializer
+    # return Response({live_settings.LIVE_KEY: live_settings.LIVE_VALUE})
+
+    # The Highly Unnecessary "Dynamic Serializer"
+    # If its is unnecessary, then Why?
+    # Because I can :p
+    LiveSerializer = type(
+        "LiveSerializer", (Serializer, ), {live_settings.LIVE_KEY: CharField()}
+    )
+
+    serializer = LiveSerializer(data={live_settings.LIVE_KEY: live_settings.LIVE_VALUE})
+    serializer.is_valid(raise_exception=True)
+    return Response(serializer.data)
